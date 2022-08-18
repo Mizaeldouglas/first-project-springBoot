fba7bb1bf80af9d5e1eb70bff84c2e4424610fed
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-08-18 14:15:26 -0300

    H2 database, test profile, JPA

--- src/main/java/com/mizaeldouglas/curso/entities/User.java
+++ src/main/java/com/mizaeldouglas/curso/entities/User.java
@@ -1,11 +1,17 @@
 package com.mizaeldouglas.curso.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
-import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -15,6 +21,7 @@ public class User implements Serializable {
 	public  User(){}
 
 	public User(Long id, String name, String email, String phone, String password) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -63,15 +70,27 @@ public class User implements Serializable {
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof User)) return false;
-		User user = (User) o;
-		return Objects.equals(getId(), user.getId());
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
-	public int hashCode() {
-		return Objects.hash(getId());
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
--- src/main/java/com/mizaeldouglas/curso/resources/UserResource.java
+++ src/main/java/com/mizaeldouglas/curso/resources/UserResource.java
@@ -12,7 +12,7 @@ public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<User> findAll() {
-		User u = new User(1L,"maria","maria@gmail.com", "99999999", "123456");
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 
