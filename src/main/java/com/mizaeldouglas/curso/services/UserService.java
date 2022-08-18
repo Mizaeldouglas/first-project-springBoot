1f7dbfa20cd60049fc2c634b44bf0d6d4eba1f1a
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-08-18 20:28:23 -0300

    Service layer, component registration

---
+++ src/main/java/com/mizaeldouglas/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.mizaeldouglas.curso.services;
+
+import com.mizaeldouglas.curso.entities.User;
+import com.mizaeldouglas.curso.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll(){
+		return  repository.findAll();
+	}
+
+	public User findById(Long id){
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
