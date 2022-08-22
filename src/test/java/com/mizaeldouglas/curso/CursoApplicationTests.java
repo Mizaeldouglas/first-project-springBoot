1870b05210710e934272df405abcde50480c1add
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-08-21 23:21:40 -0300

    update heroku version 3

--- src/test/java/com/mizaeldouglas/curso/CursoApplicationTests.java
+++ src/test/java/com/mizaeldouglas/curso/CursoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.mizaeldouglas.curso;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CursoApplicationTests {
+public class CursoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
